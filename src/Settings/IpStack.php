3a35146c258fcaa7d1df64c8639347667af1c9c6
Pablo de la Peña <p@shedcollective.org>
2021-03-25 13:58:42 +0000

    Replace settings with unified settings

---
+++ src/Settings/IpStack.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Nails\GeoIp\Driver\Settings;
+
+use Nails\Common\Helper\Form;
+use Nails\Common\Interfaces;
+use Nails\Common\Service\FormValidation;
+use Nails\Components\Setting;
+use Nails\Factory;
+
+/**
+ * Class IpStack
+ *
+ * @package Nails\GeoIp\Driver\Settings
+ */
+class IpStack implements Interfaces\Component\Settings
+{
+    const KEY_ACCESS_KEY = 'sAccessKey';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
+    public function getLabel(): string
+    {
+        return 'Geo-IP: IPInfo';
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
+    public function get(): array
+    {
+        /** @var Setting $oAccessKey */
+        $oAccessKey = Factory::factory('ComponentSetting');
+        $oAccessKey
+            ->setKey(static::KEY_ACCESS_KEY)
+            ->setType(Form::FIELD_PASSWORD)
+            ->setLabel('Access Key')
+            ->setEncrypted(true)
+            ->setValidation([
+                FormValidation::RULE_REQUIRED,
+            ]);
+
+        return [
+            $oAccessKey,
+        ];
+    }
+}
