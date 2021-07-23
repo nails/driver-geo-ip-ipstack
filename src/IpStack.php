844527da03552cdbf7980321cfa6a045def99a1e
Pablo de la Peña <p@shedcollective.org>
2021-07-23 16:05:14 +0100

    Refactor to use HttpGet class + set country/continent codes

--- src/IpStack.php
+++ src/IpStack.php
@@ -3,24 +3,32 @@
 namespace Nails\GeoIp\Driver;
 
 use Nails\Common\Driver\Base;
+use Nails\Common\Service\HttpCodes;
 use Nails\Factory;
 use Nails\GeoIp;
 use Nails\GeoIp\Exception\GeoIpDriverException;
 use Nails\GeoIp\Interfaces\Driver;
 use Nails\GeoIp\Result;
 
+/**
+ * Class IpStack
+ *
+ * @package Nails\GeoIp\Driver
+ */
 class IpStack extends Base implements Driver
 {
     /**
      * The base url of the ipstack.com service.
+     *
      * @var string
      */
-    const BASE_URL = 'http://api.ipstack.com';
+    const BASE_URL = 'https://api.ipstack.com';
 
     // --------------------------------------------------------------------------
 
     /**
      * The API Key to use.
+     *
      * @var string
      */
     protected $sAccessKey;
@@ -35,9 +43,11 @@ class IpStack extends Base implements Driver
      */
     public function lookup(string $sIp): Result\Ip
     {
-        $oHttpClient = Factory::factory('HttpClient');
-        $oIp         = Factory::factory('Ip', GeoIp\Constants::MODULE_SLUG);
 
+        /** @var \Nails\Common\Factory\HttpRequest\Get $oHttpGet */
+        $oHttpGet = Factory::factory('HttpRequestGet');
+        /** @var \Nails\GeoIp\Result\Ip $oIp */
+        $oIp = Factory::factory('Ip', GeoIp\Constants::MODULE_SLUG);
         $oIp->setIp($sIp);
 
         try {
@@ -46,61 +56,61 @@ class IpStack extends Base implements Driver
                 throw new GeoIpDriverException('An IPStack Access Key must be provided.');
             }
 
-            try {
+            $oResponse = $oHttpGet
+                ->baseUri(static::BASE_URL)
+                ->path($sIp)
+                ->query([
+                    'access_key' => $this->sAccessKey,
+                ])
+                ->execute();
 
-                $oResponse = $oHttpClient->request(
-                    'GET',
-                    static::BASE_URL . '/' . $sIp,
-                    [
-                        'query' => [
-                            'access_key' => $this->sAccessKey,
-                        ],
-                    ]
+            if ($oResponse->getStatusCode() !== HttpCodes::STATUS_OK) {
+                $oResponseBody = $oResponse->getBody();
+                throw new GeoIpDriverException(
+                    $oResponseBody->error->info ?? HttpCodes::getByCode($oResponse->getStatusCode()),
+                    $oResponseBody->error->code ?? $oResponse->getStatusCode()
                 );
-
-            } catch (\GuzzleHttp\Exception\ClientException $e) {
-                $oJson = json_decode($e->getResponse()->getBody());
-                if (!empty($oJson->error->message)) {
-                    throw new GeoIpDriverException(
-                        $oJson->error->message,
-                        $e->getCode()
-                    );
-                } else {
-                    throw new GeoIpDriverException(
-                        $e->getMessage(),
-                        $e->getCode()
-                    );
-                }
             }
 
-            $oJson = json_decode($oResponse->getBody());
+            $oResponseBody = $oResponse->getBody();
 
-            if (empty($oJson->error)) {
-
-                if (!empty($oJson->city)) {
-                    $oIp->setCity($oJson->city);
-                }
-
-                if (!empty($oJson->region_name)) {
-                    $oIp->setRegion($oJson->region_name);
-                }
-
-                if (!empty($oJson->country_name)) {
-                    $oIp->setCountry($oJson->country_name);
-                }
-
-                if (!empty($oJson->latitude)) {
-                    $oIp->setLat($oJson->latitude);
-                }
-
-                if (!empty($oJson->longitude)) {
-                    $oIp->setLng($oJson->longitude);
-                }
-            } else {
+            if (!empty($oResponseBody->error)) {
                 throw new GeoIpDriverException(
-                    $oJson->error->info,
-                    $oJson->error->code
+                    $oResponseBody->error->info,
+                    $oResponseBody->error->code
                 );
+            }
+
+            if (!empty($oResponseBody->city)) {
+                $oIp->setCity($oResponseBody->city);
+            }
+
+            if (!empty($oResponseBody->region_name)) {
+                $oIp->setRegion($oResponseBody->region_name);
+            }
+
+            if (!empty($oResponseBody->country_name)) {
+                $oIp->setCountry($oResponseBody->country_name);
+            }
+
+            if (!empty($oResponseBody->country_code)) {
+                $oIp->setCountryCode($oResponseBody->country_code);
+            }
+
+            if (!empty($oResponseBody->continent_name)) {
+                $oIp->setContinent($oResponseBody->continent_name);
+            }
+
+            if (!empty($oResponseBody->continent_code)) {
+                $oIp->setContinentCode($oResponseBody->continent_code);
+            }
+
+            if (!empty($oResponseBody->latitude)) {
+                $oIp->setLat($oResponseBody->latitude);
+            }
+
+            if (!empty($oResponseBody->longitude)) {
+                $oIp->setLng($oResponseBody->longitude);
             }
 
         } catch (\Exception $e) {
